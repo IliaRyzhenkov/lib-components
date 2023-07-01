1895612d3912ab502401b5ef26a28452c025bf2b
RyzhenkovIlia <catarsis9913@gmail.com>
2023-07-01 16:03:30 +0800

    add footer route

--- src/project/header/index.tsx
+++ src/project/header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import style  '../header/index.module.css'
+import style from  './index.module.css'
 
 
 const Header = () => {
